019074ced4ac33da13920c351ab1615ec6890d03
Mark Rublevskiyy <57556412+Mark-Rublevskiy@users.noreply.github.com>
2020-10-08 18:21:19 +0300

    site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-$(".slider").slick();
+$(".slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fal fa-chevron-left'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fal fa-chevron-right'></i></button>"
+})
